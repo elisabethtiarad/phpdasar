d821b4c076e568ebc8e80510f41f33ff69b9565a
elisabethtiarad <elisabethtiarad@gmail.com>
2022-11-12 23:04:06 +0700

    php

---
+++ pertemuan2/index.php
@@ -0,0 +1,52 @@
+<?php
+// Pertemuan 2- PHP Dasar
+/*
+sintaks PHP
+Standar output
+echo, print
+print_r = mencetak array
+var_dump = melihat isi variable
+*/
+
+//echo "Eliabeth Tiara Dewanti";
+//var_dump("Eliabeth Tiara Dewanti");
+//Penulisan sintaks PHP
+//1. PHP di dalam html
+//2. Html di dalam php
+
+//variable : tidak boleh diawali dengan angka, tapi boleh mengandung angka 
+//$nama = "Eli"
+
+//operator
+//+ - * / %
+
+//penggabung string
+// $nama_depan = "Elisabeth";
+// $nama_belakang = "Dewanti";
+// echo $nama_depan . " " . $nama_belakang;
+
+//assigment
+//= += -+ *= /= %= .=
+
+//perbandingan
+//< > <= >= == !=
+
+//indentitas
+//=== !==
+
+//LOGIKA
+// && || !
+?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hallo selamat datang </h1>
+</body>
+</html> -->
