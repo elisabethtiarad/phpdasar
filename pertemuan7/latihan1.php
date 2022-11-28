5f54923229eb02370f33d1333e85eb8f39b80530
elisabethtiarad <elisabethtiarad@gmail.com>
2022-11-28 11:52:32 +0700

    belajar

--- pertemuan7/latihan1.php
+++ pertemuan7/latihan1.php
@@ -1,4 +1,46 @@
-<?php 
-//superglobal = merupakan array asosiatif
-//$_GET
-$_GET["nama"] = "Elisabeth Tiara";
+<?php
+$mahasiswa = [
+    [
+        "nama" => "Elisabeth Tiara",
+        "nim" => "205314143",
+        "prodi" => "Informatika",
+        "email" => "elisabethtiarad@gmail.com",
+        "gambar" => "elitiara.jpeg"
+    ],
+    [
+        "nama" => "ETiaraeli",
+        "nim" => "205314144",
+        "prodi" => "Teknik Kimia",
+        "email" => "tiaraeli@gmail.com",
+        "gambar" => "muel.jpg"
+    ]
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GET</title>
+</head>
+
+<body>
+    <h1>Daftar mahasiswa</h1>
+    <ul>
+        <?php foreach ($mahasiswa as $mhs) : ?>
+
+        <li>
+            <a
+                href="latihan2.php?nama=<?= $mhs["nama"]; ?>&nim=<?= $mhs["nim"]; ?>&prodi=<?= $mhs["prodi"]; ?>&email=<?= $mhs["email"]; ?>&gambar=<?= $mhs["gambar"]; ?>"><?= $mhs["nama"];  ?>
+            </a>
+        </li>
+
+        <?php endforeach;  ?>
+    </ul>
+
+</body>
+
+</html>
---
+++ pertemuan7/latihan2.php
@@ -0,0 +1,35 @@
+<?php
+if (
+    !isset($_GET["nama"]) ||
+    !isset($_GET["nim"]) ||
+    !isset($_GET["prodi"]) ||
+    !isset($_GET["email"]) ||
+    !isset($_GET["gambar"])
+) {
+    header("Location: latihan1.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail mahasiswa</title>
+</head>
+
+<body>
+    <ul>
+        <li><img src="img/<?= $_GET["gambar"];  ?>" alt=""></li>
+        <li><?= $_GET["nama"];  ?></li>
+        <li><?= $_GET["nim"];  ?></li>
+        <li><?= $_GET["prodi"];  ?></li>
+        <li><?= $_GET["email"];  ?></li>
+    </ul>
+    <a href="latihan1.php">Kembali</a>
+</body>
+
+</html>
