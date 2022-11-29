5c4d99e8d9ee60ccb09c64b2b1566ae37f3b2dbb
elisabethtiarad <elisabethtiarad@gmail.com>
2022-11-30 01:18:05 +0700

    belajar pertemuan 13

---
+++ pertemuan11/index.php
@@ -0,0 +1,59 @@
+<?php
+//koneksi ke database
+
+require 'functions.php'; //memanggil file function
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <a href="tambah.php">Tambah Data Mahasiswa</a>
+    <br>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Jurusan</th>
+            <th>Email</th>
+
+        </tr>
+        <?php $i = 1; ?>
+
+        <?php foreach ($mahasiswa as $row) : ?>
+
+        <tr>
+            <td><?= $i;   ?>
+            </td>
+            <td><a href="ubah.php?id=<?= $row["id"]; ?>">Ubah</a> | <a href="hapus.php?id=<?= $row["id"]; ?>"
+                    onclick="return confirm ('Apakah Yakin menghapus?')">Hapus</a></td>
+
+            <td><img src="img/<?= $row["gambar"]; ?>" width="50" alt=""></td>
+            <td><?= $row["nim"]; ?> </td>
+            <td><?= $row["nama"]; ?> </td>
+            <td><?= $row["jurusan"]; ?> </td>
+            <td><?= $row["email"]; ?></td>
+        </tr>
+        <?php $i++;  ?>
+
+        <?php endforeach;  ?>
+
+    </table>
+</body>
+
+</html>
